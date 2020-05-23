4fd1fcd24084c50182cffb3005a2f50c6c07772b
Artem Popov <aptem336@gmail.com>
2020-05-24 03:15:02 +0800

    converters start

--- src/main/java/controller/MakerController.java
+++ src/main/java/controller/MakerController.java
@@ -2,13 +2,16 @@ package controller;
 
 import entity.Maker;
 
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import java.util.List;
 
 @Named
-public class MakerController implements Controller<Maker> {
+public class MakerController implements Controller<Maker>, Converter<Maker> {
     public static final String getAllNamedQuery = "getAllMakers";
     @Inject
     private EntityManager em;
@@ -16,5 +19,15 @@ public class MakerController implements Controller<Maker> {
     @Override
     public List<Maker> getAll() {
         return em.createNamedQuery(getAllNamedQuery, Maker.class).getResultList();
+    }
+
+    @Override
+    public Maker getAsObject(FacesContext facesContext, UIComponent uiComponent, String s) {
+        return null;
+    }
+
+    @Override
+    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Maker maker) {
+        return maker.getName();
     }
 }
